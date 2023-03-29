dffd25bda819f9f0b649b7379abe906b37186336
Ikhsan Fajar Nugroho <59926991+ikhsannugs@users.noreply.github.com>
2023-03-29 20:51:20 +0700

    Update landing-page.sh

--- landing-page.sh
+++ landing-page.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 echo "Menyiapkan Installasi Web server"
-sudo apt-get update
+apt-get update
 echo "Melakukan Installasi Webserver"
-sudo apt-get install -y apache2 zip unzip
+apt-get install -y apache2 zip unzip
 echo "Download file apps landing page"
 wget https://github.com/sdcilsy/landing-page/archive/refs/heads/master.zip
 echo "Memindahkan data"
-sudo rm /var/www/html/*
-sudo rm -R /var/www/html/*
-sudo mv landing-page/* /var/www/html
+unzip master.zip
+rm -rf /var/www/html/* | mv landing-page/* /var/www/html
 echo "restart service apache2"
 systemctl restart apache2
